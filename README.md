13c35b7a4c3b73271d80eb7216603a937210323e
Ziang Liu <ziang_liu@outlook.com>
2021-08-02 23:36:08 -0700

    Add simple ompl python demo

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 * https://ompl.kavrakilab.org/install-ompl-ubuntu.sh
 * ```chmod u+x install-ompl-ubuntu.sh```
 * ```./install-ompl-ubuntu.sh --python```
-  * Python bindings are installed under ompl-1.5.2/py-bindings/ompl
+  * Python bindings are installed under ```ompl-1.5.2/py-bindings/ompl```
 * Add ompl path to system PATH
   * ```
     from os.path import abspath, dirname, join
@@ -12,9 +12,8 @@
     sys.path.insert(0, join('/home', 'ziang', 'Workspace', 'lib', 'ompl-1.5.2', 'py-bindings'))
     ```
 ### Without Python Bindings
-* ```sudo apt-get install libompl-dev ompl-demos``` No python bindings!
-## Steps
+* ```sudo apt-get install libompl-dev ompl-demos```
+## Backtrack Notes
 * Removed realsense repo from sources.list, because the server was down, blocking apt update
 * https://docs.google.com/document/d/1_ncHlFyim1gMGIV_77-Pti_m1WyNrC6Vt1UF7WtFxHM/edit#
-* ```export PYTHONPATH="~/Workspace/lib/ompl-1.5.2/py-bindings/:$PYTHONPATH"```
 
---
+++ ompl_planning_demo.py
@@ -0,0 +1,73 @@
+
+#!/usr/bin/python3
+
+
+from os.path import abspath, dirname, join
+import sys
+sys.path.insert(0, join('/home', 'ziang', 'Workspace', 'lib', 'ompl-1.5.2', 'py-bindings'))
+# print(sys.path)
+from ompl import base as ob
+from ompl import geometric as goem_planners
+
+import time
+
+def isStateValid(state):
+    return state[0] < .9
+
+def plan():
+    # create astate space
+    space = ob.RealVectorStateSpace(7)
+    # set state limits
+    bounds = ob.RealVectorBounds(7)
+    bounds_lower = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    bounds_upper = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+    for i in range(7):
+      bounds.setLow(i, bounds_lower[i])
+      bounds.setHigh(i, bounds_upper[i])
+    space.setBounds(bounds)
+
+    # construct an instance of space information from this state space
+    si = ob.SpaceInformation(space)
+    # set state validity checking for this space
+    si.setStateValidityChecker(ob.StateValidityCheckerFn(isStateValid))
+    # create a random start state
+    start = ob.State(space)
+    start.random()
+    start[0] = 0.1
+    # create a random goal state
+    goal = ob.State(space)
+    goal.random()
+    # create a problem instance
+    pdef = ob.ProblemDefinition(si)
+    # set the start and goal states
+    pdef.setStartAndGoalStates(start, goal)
+    # create a planner for the defined space
+    planner = goem_planners.RRTstar(si)
+    # set the problem we are trying to solve for the planner
+    planner.setProblemDefinition(pdef)
+    # perform setup steps for the planner
+    planner.setup()
+    # print the settings for this space
+    print(si.settings())
+    # print the problem settings
+    print(pdef)
+    # attempt to solve the problem within one second of planning time
+    t_1 = time.perf_counter()
+    solved = planner.solve(2.0)
+    t_2 = time.perf_counter()
+
+    if solved:
+        # get the goal representation from the problem definition (not the same as the goal state)
+        # and inquire about the found path
+        path = pdef.getSolutionPath()
+        print("Found solution:\n%s" % path)
+        print(f"Time: {t_2 - t_1:0.4f}s")
+        print("Solution Cost: %f\n" %path.length())
+    else:
+        print("No solution found")
+
+
+
+if __name__ == "__main__":
+    print("")
+    plan()
